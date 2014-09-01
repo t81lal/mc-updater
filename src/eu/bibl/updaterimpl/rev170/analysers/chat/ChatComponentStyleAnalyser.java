bb3343914653e7477df8a9586b7a2068a0cb0c86
Bibl <GenerallyCool@hotmail.com>
2014-09-01 01:49:31 +0100

    Updated ByteAnalysis lib with typo fix in HookMap.

--- src/eu/bibl/updaterimpl/rev170/analysers/chat/ChatComponentStyleAnalyser.java
+++ src/eu/bibl/updaterimpl/rev170/analysers/chat/ChatComponentStyleAnalyser.java
@@ -1,4 +1,12 @@
 package eu.bibl.updaterimpl.rev170.analysers.chat;
+
+import eu.bibl.banalysis.analyse.Analyser;
+import eu.bibl.banalysis.storage.ClassMappingData;
+import eu.bibl.banalysis.storage.HookMap;
+import eu.bibl.banalysis.storage.InterfaceMappingData;
+import eu.bibl.banalysis.storage.classes.ClassContainer;
+import eu.bibl.updaterimpl.rev170.analysers.MinecraftAnalyser;
+
 public class ChatComponentStyleAnalyser extends Analyser {
 	
 	public ChatComponentStyleAnalyser(ClassContainer container, HookMap hookMap) {
@@ -6,15 +14,15 @@ public class ChatComponentStyleAnalyser extends Analyser {
 	}
 	
 	@Override
-public boolean accept() {
-		ClassMappingData owner = hookMap.getClassByObfuscatedName(cn.name);
+	public boolean accept() {
+		ClassMappingData owner = hookMap.getClassByOfuscatedName(cn.name);
 		if (owner == null)
 			return false;
 		return owner.getRefactoredName().equals("ChatComponentStyle");
 	}
 	
 	@Override
-public InterfaceMappingData run() {
+	public InterfaceMappingData run() {
 		ClassMappingData chatComponent = new ClassMappingData((String) (cn.interfaces.get(0)), "ChatComponent");
 		hookMap.addClass(chatComponent);
 		
